c87b184b135f1cc5efeed8a777f561e3428ea98b
Yunus Eren Guzel <exculuber@gmail.com>
2015-09-11 16:03:08 +0300

    version 0.0.2

--- lib/HaliciPaymentConnector.rb
+++ lib/HaliciPaymentConnector.rb
@@ -12,8 +12,8 @@ class HaliciPaymentConnector
   end
 
 
-  def check(payment_id)
-    RestClient.post("#{host}/payments/check", {payment_id:payment_id,token:token}) do |response, request, result|
+  def check(payment_token)
+    RestClient.post("#{host}/payments/check", {payment_token:payment_token,token:token}) do |response, request, result|
       return JSON.parse(result.body)['payment']
     end
   end
